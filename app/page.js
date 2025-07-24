ef235ce76ea58326c4a97077f2d498ee9ff79021
Rahul <121254290+rahul2271@users.noreply.github.com>
2025-07-24 12:03:09 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,11 +8,11 @@ export const metadata = {
     title: "RC Tech Solutions | Turning Ideas into Digital Reality",
     description:
       "Custom websites, SEO, branding, and marketing solutions tailored to your business. Experience premium digital transformation with RC Tech Solutions.",
-    url: "https://www.rctechsolutions.com",
+    url: "https://rctechsolutions.com",
     siteName: "RC Tech Solutions",
     images: [
       {
-        url: "https://www.rctechsolutions.com/rclogo.png",
+        url: "https://rctechsolutions.com/rclogo.png",
         width: 1200,
         height: 630,
         alt: "RC Tech Solutions Banner",
@@ -25,7 +25,7 @@ export const metadata = {
     title: "RC Tech Solutions",
     description:
       "Luxury-grade web development and branding by RC Tech Solutions.",
-    images: ["https://www.rctechsolutions.com/rclogo.png"],
+    images: ["https://rctechsolutions.com/rclogo.png"],
   },
 };
 

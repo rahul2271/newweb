c1b338e138a9f12aac8886ccfe833e22569920b5
Rahul <121254290+rahul2271@users.noreply.github.com>
2025-11-11 11:39:46 +0530

    Update page.js

--- app/homepage/page.js
+++ app/homepage/page.js
@@ -14,7 +14,7 @@ import LatestBlogs from "../components/LatestBlogs";
 export const metadata = {
   title: "Best Software Development Company for Digital Success | RC tech Solutions",
   description: "Your vision, our expertise. As the best software development company, we design and develop custom software that enhances productivity and maximizes business efficiency.",
-  metadataBase: new URL("https://www.rctechsolutions.com"),
+  metadataBase: new URL("https://www.rctechsolutions.com/"),
   // alternates: {
   //   canonical: "/",
   // },

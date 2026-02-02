b8b77eef4cb2c372fb8eb26369ad1af8fc82d536
Rahul <121254290+rahul2271@users.noreply.github.com>
2026-02-02 15:34:41 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -160,37 +160,54 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         {/* Razorpay Script */}
-        <Script src="https://checkout.razorpay.com/v1/checkout.js" strategy="afterInteractive" />
+        <Script
+          src="https://checkout.razorpay.com/v1/checkout.js"
+          strategy="afterInteractive"
+        />
       </head>
+
       <body className={`${poppins.variable} antialiased`}>
-        {/* GTM, GA, and FB Pixel Scripts */}
+        {/* Google Tag Manager */}
         <Script id="gtm-init" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-KQBSZ2Z9');`}
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KQBSZ2Z9');
+          `}
         </Script>
 
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-YLHN7WV840" />
+        {/* Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-YLHN7WV840"
+        />
         <Script id="gtag-init" strategy="afterInteractive">
-          {`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-YLHN7WV840');`}
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YLHN7WV840');
+          `}
         </Script>
 
+        {/* Meta / Facebook Pixel (Multiple Pixels) */}
         <Script id="fb-pixel" strategy="afterInteractive">
-          {`!function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1925925141537683');
-          fbq('track', 'PageView');`}
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '1925925141537683');
+            fbq('init', '2322739851486802');
+            fbq('track', 'PageView');
+          `}
         </Script>
 
         {/* GTM NoScript */}
@@ -200,10 +217,10 @@ export default function RootLayout({ children }) {
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
+          />
         </noscript>
 
-        {/* Facebook Pixel NoScript */}
+        {/* Meta Pixel NoScript */}
         <noscript>
           <img
             height="1"
@@ -213,6 +230,16 @@ export default function RootLayout({ children }) {
           />
         </noscript>
 
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2322739851486802&ev=PageView&noscript=1"
+          />
+        </noscript>
+
+        {/* Layout Components */}
         <CustomCursor />
         <AdvancedHeader />
         {children}
@@ -221,4 +248,3 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
-

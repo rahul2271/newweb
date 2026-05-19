3bf620229f3fab02f90f8f85531b8b1039c478c1
Rahul <121254290+rahul2271@users.noreply.github.com>
2026-05-19 15:40:23 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,25 @@
-const nextConfig = { images: { domains: [ "images.pexels.com", "images.unsplash.com", "source.unsplash.com", "plus.unsplash.com", "api.uifaces.co", "randomuser.me", "avatars.githubusercontent.com",'upload.wikimedia.org', 'cdn.shopify.com', 'd1.awsstatic.com', "pagedone.io", "avatars.githubusercontent.com",'cdn.pixabay.com','pagedone.io','miro.medium.com', 'www.investopedia.com', 'img.freepik.com','firebasestorage.googleapis.com' ], }, }; export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "images.pexels.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "source.unsplash.com" },
+      { protocol: "https", hostname: "plus.unsplash.com" },
+      { protocol: "https", hostname: "api.uifaces.co" },
+      { protocol: "https", hostname: "randomuser.me" },
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+      { protocol: "https", hostname: "upload.wikimedia.org" },
+      { protocol: "https", hostname: "cdn.shopify.com" },
+      { protocol: "https", hostname: "d1.awsstatic.com" },
+      { protocol: "https", hostname: "pagedone.io" },
+      { protocol: "https", hostname: "cdn.pixabay.com" },
+      { protocol: "https", hostname: "miro.medium.com" },
+      { protocol: "https", hostname: "www.investopedia.com" },
+      { protocol: "https", hostname: "img.freepik.com" },
+      { protocol: "https", hostname: "firebasestorage.googleapis.com" },
+    ],
+  },
+};
+
+export default nextConfig;
